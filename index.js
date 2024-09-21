f3e2a58939abca18aa995ff20331db7b5e9a936e
Emily Morgan <120770144+emilykmorgan@users.noreply.github.com>
2024-09-21 19:50:20 -0400

    Using npm package 'multer' to access properties of uploaded file

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
+require('dotenv').config();
+
+// Using an npm package called multer in order to work with file uploads from an HTML form
+const multer  = require('multer');
+const upload = multer({ dest: 'uploads/' });
 
 var app = express();
-
 app.use(cors());
+
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  // Access the uploaded file
+  let file = req.file;
 
-
+  // Return the JSON response with file properties
+  res.json({
+    name: file.originalname,
+    type: file.mimetype,
+    size: file.size
+  });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
